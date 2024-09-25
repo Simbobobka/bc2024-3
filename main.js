715cfdd8de309a4cbb68155f56c28e5354cbff77
Simbobobka bc2024-3 <tomashuk.oleg2005@gmail.com>
2024-09-25 20:14:26 +0300

    task1v.2.0

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { program } = require('commander');
 const fs = require('fs');
 
+// Функція для читання вхідного файлу
 function readInputFile(filePath) {
     if (!fs.existsSync(filePath)) {
         console.error("Cannot find input file");
@@ -11,11 +12,13 @@ function readInputFile(filePath) {
     return JSON.parse(data);
 }
 
+// Функція для запису у файл
 function writeOutputFile(filePath, data) {
     fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf8');
     console.log(`Result written to ${filePath}`);
 }
 
+// Налаштування аргументів командного рядка
 program
     .requiredOption('-i, --input <path>', 'Path to input JSON file')
     .option('-o, --output <path>', 'Path to output file')
@@ -25,23 +28,26 @@ program.parse(process.argv);
 
 const options = program.opts();
 
-if (!options.input) {
-    console.error("Please, specify input file");
+// Перевірка, чи існує вхідний файл
+if (!fs.existsSync(options.input)) {
+    console.error("Cannot find input file");
     process.exit(1);
 }
 
+// Читання вхідного файлу
 const inputData = readInputFile(options.input);
 
-
+// Виведення в консоль, якщо задано параметр -d
 if (options.display) {
     console.log("Data:", inputData);
 }
 
-
+// Запис у файл, якщо задано параметр -o
 if (options.output) {
     writeOutputFile(options.output, inputData);
 }
 
+// Якщо не задано жодного з параметрів -o або -d, програма не повинна виводити нічого
 if (!options.output && !options.display) {
     process.exit(0);
 }
